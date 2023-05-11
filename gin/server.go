62d83d3959af47f1b6cb9a025830b41fcee20e4d
Eduardo Barbosa <eduardo.barbosa@adevinta.com>
2023-05-11 11:47:40 +0200

    start gin tutorial

--- gin/server.go
+++ gin/server.go
@@ -1,1 +1,15 @@
-package gin
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/health", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"status": "UP",
+		})
+	})
+
+	server.Run(":8080")
+}
