741fadfa3400c07cdca65a101a40ae74c7fcb4be
Eduardo Barbosa <eduardo.barbosa@adevinta.com>
2023-05-15 12:21:21 +0200

    Bechmark testing
    Co-authored-by: Eduardo Barbosa <eduardob907@gmail.com>

---
+++ lessons/linkedin-hands-on/testing/benchmark/benchmark.go
@@ -0,0 +1,28 @@
+package main
+
+type numeric interface {
+	~int | ~float64
+}
+
+func sum[T numeric](numbers ...T) T {
+	var s T
+	for _, n := range numbers {
+		s += n
+	}
+	return s
+}
+
+func sumAny(numbers []interface{}) interface{} {
+	var result float64
+	for _, n := range numbers {
+		switch n.(type) {
+		case int:
+			result += float64(n.(int))
+		case float32, float64:
+			result += n.(float64)
+		default:
+			continue
+		}
+	}
+	return result
+}
