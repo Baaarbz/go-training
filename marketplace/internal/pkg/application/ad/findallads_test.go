ac9469a9ec119fbe5dde0d1d7c19c821eb37e5a4
Eduardo Barbosa <eduardo.barbosa@adevinta.com>
2023-05-18 18:01:55 +0200

    Testing: Add errors
    Co-authored-by: Eduardo Barbosa <eduardob907@gmail.com>

--- marketplace/internal/pkg/application/ad/findallads_test.go
+++ marketplace/internal/pkg/application/ad/findallads_test.go
@@ -15,10 +15,11 @@ func TestFindAllAds_Execute_GetListOfAds(t *testing.T) {
 	anId, _ := valueobject.NewId("574cc928-f4bd-11ed-ad0e-8a6a68a798d6")
 	ad := NewAd("Simple title", "Simple ad description for testing", 20)
 	ad.SetId(anId)
-	ads.EXPECT().FindAllAds().Return([]Ad{ad})
+	ads.EXPECT().FindAllAds().Return([]Ad{ad}, nil)
 
-	gotAds := service.Execute()
+	gotAds, err := service.Execute()
 
 	assert.True(t, len(gotAds) == 1)
+	assert.Nil(t, err)
 	assert.Equal(t, []GetAdsResponse{{anId.String()}}, gotAds)
 }
