9b2928cf405832751731c87e37ea1809376d3294
Eduardo Barbosa <eduardo.barbosa@adevinta.com>
2023-05-18 17:50:01 +0200

    Testing: Add SaveAd tests
    Co-authored-by: Eduardo Barbosa <eduardob907@gmail.com>

---
+++ marketplace/internal/pkg/application/ad/savead_test.go
@@ -0,0 +1,52 @@
+package ad
+
+import (
+	. "barbz.dev/marketplace/internal/pkg/domain/ad"
+	"barbz.dev/marketplace/internal/pkg/domain/ad/mocks"
+	. "barbz.dev/marketplace/pkg/valueobject"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestSaveAd_Execute_Success(t *testing.T) {
+	ads := mocks.NewAdRepository(t)
+	service := NewSaveAd(ads)
+
+	requestAd := SaveAdRequest{
+		Title:       "Test save ad success",
+		Description: "Description to save successfully an ad",
+		Price:       99.99,
+	}
+	savedAd := NewAd(Title(requestAd.Title), Description(requestAd.Description), Price(requestAd.Price))
+	savedAd.SetId("test-id")
+	ads.EXPECT().SaveAd(mock.AnythingOfType("Ad")).Return(savedAd)
+
+	gotAd, _ := service.Execute(requestAd)
+
+	assert.Equal(t, SaveAdResponse{savedAd.GetId().String()}, gotAd)
+}
+func TestSaveAd_Execute_FailValidation(t *testing.T) {
+	ads := mocks.NewAdRepository(t)
+	type args struct {
+		request SaveAdRequest
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    SaveAdResponse
+		wantErr bool
+	}{
+		{"wrong Title format", args{request: SaveAdRequest{Title: ":(", Description: "Description to pass the validation", Price: 0}}, SaveAdResponse{}, true},
+		{"wrong Title format", args{request: SaveAdRequest{Title: "Title to pass validation", Description: ":(", Price: 0}}, SaveAdResponse{}, true},
+		{"wrong Title format", args{request: SaveAdRequest{Title: "Title to pass validation", Description: "Description to pass the validation", Price: -20}}, SaveAdResponse{}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			service := SaveAd{ads: ads}
+			gotSavedAd, err := service.Execute(tt.args.request)
+			assert.Equal(t, SaveAdResponse{}, gotSavedAd)
+			assert.Error(t, err)
+		})
+	}
+}
