a308d7d4e18e6029ba9324a2996c449fae75664c
Eduardo Barbosa <eduardo.barbosa@adevinta.com>
2023-05-18 17:26:20 +0200

    Testing: Add test FindAllAds
    Co-authored-by: Eduardo Barbosa <eduardob907@gmail.com>

---
+++ marketplace/internal/pkg/application/ad/findallads_test.go
@@ -0,0 +1,24 @@
+package ad
+
+import (
+	. "barbz.dev/marketplace/internal/pkg/domain/ad"
+	"barbz.dev/marketplace/internal/pkg/domain/ad/mocks"
+	"barbz.dev/marketplace/pkg/valueobject"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFindAllAds_Execute_GetListOfAds(t *testing.T) {
+	ads := mocks.NewAdRepository(t)
+	service := NewFindAllAds(ads)
+
+	anId, _ := valueobject.NewId("574cc928-f4bd-11ed-ad0e-8a6a68a798d6")
+	ad := NewAd("Simple title", "Simple ad description for testing", 20)
+	ad.SetId(anId)
+	ads.EXPECT().FindAllAds().Return([]Ad{ad})
+
+	gotAds := service.Execute()
+
+	assert.True(t, len(gotAds) == 1)
+	assert.Equal(t, []GetAdsResponse{{anId.String()}}, gotAds)
+}
