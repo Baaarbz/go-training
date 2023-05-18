0f8cc868fa82ef4fd499b77065681fc76ad5759b
Eduardo Barbosa <eduardo.barbosa@adevinta.com>
2023-05-18 14:01:54 +0200

    Testing: InMemoryRepository
    Co-authored-by: Eduardo Barbosa <eduardob907@gmail.com>

--- marketplace/cmd/marketplace/bootstrap/bootstrap.go
+++ marketplace/cmd/marketplace/bootstrap/bootstrap.go
@@ -8,7 +8,7 @@ import (
 
 func Run() {
 	saveAd, findAllAds, findAdById := initAdsDependencies()
-	runMocks(saveAd, findAllAds, findAdById)
+	log.Printf("%v\t%v\t%v", saveAd, findAllAds, findAdById)
 }
 
 func initAdsDependencies() (save SaveAd, findAll FindAllAds, findById FindAdById) {
@@ -17,39 +17,4 @@ func initAdsDependencies() (save SaveAd, findAll FindAllAds, findById FindAdById
 	findAll = NewFindAllAds(ads)
 	findById = NewFindAdById(ads)
 	return
-}
-
-func runMocks(saveAd SaveAd, findAllAds FindAllAds, findAdById FindAdById) {
-	ad := SaveAdRequest{
-		Title:       "Sample Ad",
-		Description: "This is the description to test the app",
-		Price:       29.99,
-	}
-	response, _ := saveAd.Execute(ad)
-	addMocksToRepository(saveAd)
-
-	if adFound, err := findAdById.Execute(response.Id); err != nil {
-		log.Fatalf("exception: %v\n", err)
-	} else {
-		log.Println(adFound)
-	}
-
-	allAds := findAllAds.Execute()
-	log.Println("Size of the array:", len(allAds))
-	log.Println(allAds)
-}
-
-func addMocksToRepository(saveAd SaveAd) {
-	ad := SaveAdRequest{Title: "Sample Ad", Description: "This is the description to test the app", Price: 29.99}
-	saveAd.Execute(ad)
-	ad = SaveAdRequest{Title: "BMW 320D", Description: "This is the description to test the app", Price: 1800.99}
-	saveAd.Execute(ad)
-	ad = SaveAdRequest{Title: "TV 45' Sony", Description: "This is the description to test the app", Price: 399.99}
-	saveAd.Execute(ad)
-	ad = SaveAdRequest{Title: "Sportiva Rock Climbing Foot", Description: "This is the description to test the app", Price: 70.00}
-	saveAd.Execute(ad)
-	ad = SaveAdRequest{Title: "Macbook Pro 16'", Description: "This is the description to test the app", Price: 1500.00}
-	saveAd.Execute(ad)
-	ad = SaveAdRequest{Title: "Rolex limited edition", Description: "This is the description to test the app", Price: 29999.99}
-	saveAd.Execute(ad)
 }
---
+++ marketplace/internal/infrastructure/repository/ad/inmemoryrepository_test.go
@@ -0,0 +1,100 @@
+package ad
+
+import (
+	. "barbz.dev/marketplace/internal/pkg/domain/ad"
+	. "barbz.dev/marketplace/pkg/valueobject"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestInMemoryRepository_FindAdById(t *testing.T) {
+	anId, _ := NewId("574cc928-f4bd-11ed-ad0e-8a6a68a798d6")
+	anAd := mockAdWithId(anId)
+
+	type fields struct {
+		ads []Ad
+	}
+	type args struct {
+		id AdId
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantAd  Ad
+		wantErr bool
+	}{
+		{"empty slice should error", fields{ads: []Ad{}}, args{id: anId}, Ad{}, true},
+		{"not find ad by id should error", fields{ads: generateSliceMockAds()}, args{id: anId}, Ad{}, true},
+		{"slice with values should find ad", fields{ads: append(generateSliceMockAds(), anAd)}, args{id: anId}, anAd, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repository := &InMemoryRepository{
+				ads: tt.fields.ads,
+			}
+			gotAd, err := repository.FindAdById(tt.args.id)
+
+			assert.Equal(t, tt.wantAd, gotAd)
+			switch tt.wantErr {
+			case true:
+				assert.NotNil(t, err)
+			case false:
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
+
+func TestInMemoryRepository_FindAllAds(t *testing.T) {
+	sliceAdsMock := generateSliceMockAds()
+	type fields struct {
+		ads []Ad
+	}
+	tests := []struct {
+		name           string
+		fields         fields
+		wantAdResponse []Ad
+	}{
+		{"empty ads slice should return empty slice", fields{ads: []Ad{}}, []Ad{}},
+		{"slice with 4 elements should return 4 elements", fields{ads: sliceAdsMock[:4]}, sliceAdsMock[:4]},
+		{"slice with more than 5 elements should return 5 elements", fields{ads: sliceAdsMock}, sliceAdsMock[:5]},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repository := &InMemoryRepository{
+				ads: tt.fields.ads,
+			}
+			gotAdResponse := repository.FindAllAds()
+			assert.Equal(t, tt.wantAdResponse, gotAdResponse)
+		})
+	}
+}
+
+func TestInMemoryRepository_SaveAd(t *testing.T) {
+	anAd := NewAd("Test Save", "Test save Ad description mock", 10)
+	repository := &InMemoryRepository{ads: []Ad{}}
+
+	expectedAd := repository.SaveAd(anAd)
+
+	gotAd, _ := repository.FindAdById(expectedAd.GetId())
+	assert.Equal(t, expectedAd, gotAd)
+}
+
+func generateSliceMockAds() (mockAds []Ad) {
+	mockAds = make([]Ad, 0)
+	mockAds = append(mockAds, NewAd("Sample Ad", "This is the description to test the app", 30))
+	mockAds = append(mockAds, NewAd("TV 45' Sony", "This is the description to test the app", 599.99))
+	mockAds = append(mockAds, NewAd("Sportiva Rock Climbing Foot", "This is the description to test the app", 70))
+	mockAds = append(mockAds, NewAd("Macbook Pro 16'", "This is the description to test the app", 1799.00))
+	mockAds = append(mockAds, NewAd("Rolex limited edition", "This is the description to test the app", 100500))
+	mockAds = append(mockAds, NewAd("Northface t-shirt", "This is the description to test the app", 55))
+
+	return
+}
+func mockAdWithId(anId AdId) Ad {
+	adWithId := NewAd("BMW 320D", "This is the description to test the app", 18500)
+	adWithId.SetId(anId)
+
+	return adWithId
+}
