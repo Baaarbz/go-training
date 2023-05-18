5dd4d8fc90fec8f2fd27c76a71c14a5b9137c6e4
Eduardo Barbosa <eduardo.barbosa@adevinta.com>
2023-05-18 17:17:51 +0200

    Testing: Add mockery
    Co-authored-by: Eduardo Barbosa <eduardob907@gmail.com>

--- marketplace/internal/pkg/domain/ad/adrepository.go
+++ marketplace/internal/pkg/domain/ad/adrepository.go
@@ -2,6 +2,7 @@ package ad
 
 import "barbz.dev/marketplace/pkg/valueobject"
 
+//go:generate mockery --name=AdRepository --filename mock_adrepository.go
 type AdRepository interface {
 	SaveAd(ad Ad) Ad
 	FindAdById(id valueobject.AdId) (Ad, error)
